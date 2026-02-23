3df932a453874fd77de4b2c09b318311d47e9ecf
FatBoiiChang <reapersguild1904@gmail.com>
2026-02-23 14:09:14 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+// Simple WebSocket multiplayer server
+const WebSocket = require("ws");
+const server = new WebSocket.Server({ port: 8080 });
+
+let players = {};
+
+server.on("connection", (socket) => {
+  const id = Math.random().toString(36).substr(2, 9);
+  players[id] = { x: 1000, y: 1000 };
+
+  socket.send(JSON.stringify({ type: "init", id, players }));
+
+  socket.on("message", (msg) => {
+    const data = JSON.parse(msg);
+
+    if (data.type === "move") {
+      players[id] = { x: data.x, y: data.y };
+
+      // Broadcast to all players
+      server.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "update", id, x: data.x, y: data.y }));
+        }
+      });
+    }
+  });
+
+  socket.on("close", () => {
+    delete players[id];
+    server.clients.forEach((client) => {
+      client.send(JSON.stringify({ type: "remove", id }));
+    });
+  });
+});
+
+console.log("Multiplayer server running on ws://localhost:8080");
